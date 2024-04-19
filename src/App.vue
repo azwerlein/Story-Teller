f98a42910751c2925f02ca47ef1a0ee7d932223a
Burning Cactus <azwerlein77@gmail.com>
2024-04-19 17:51:32 -0500

    First commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="flex border-2 border-slate-50">
+    <h2 class="text-primary me-2">This is a Tailwind div</h2>
+    <button class="btn btn-primary">DaisyUI Button</button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
