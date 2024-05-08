e65cfc9b9026e24ded6de0819c027c52c905e253
azwerlein <azwerlein@my.wctc.edu>
2024-05-07 21:09:21 -0500

    User card profile pictures

--- src/components/UserCard.vue
+++ src/components/UserCard.vue
@@ -20,7 +20,13 @@ export default {
   <RouterLink :to="{name: 'profile', params: {id: this.id}}">
     <div class="card bg-neutral-600 text-black border-4 border-amber-300">
       <div class="card-body">
-        <h2>{{ profile.displayName }}</h2>
+        <div class="grid grid-cols-3">
+          <img :src="profile.photoURL" alt="Profile">
+          <div class="col-span-2">
+            <h2 class="">{{ profile.displayName }}</h2>
+          </div>
+        </div>
+
       </div>
     </div>
   </RouterLink>
